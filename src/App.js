fba46c86a1ecb8589ad6a5a11c4f2ede172d8425
大彬哥666 <leolau2012>
2019-01-25 15:49:39 +0800

    首次提交
    增加了ES6、react编译，简单能看懂的工作流

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+  return (
+    <div>
+      <p>大彬哥一如既往的666</p>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
